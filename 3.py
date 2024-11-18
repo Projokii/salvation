0d8657d1042abead45e76446a7bc4032bc085847
Projokii <141165642+Projokii@users.noreply.github.com>
2024-11-18 21:05:53 +0700

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,9 @@
+import asyncio
+from proxy_handler import main
+
+async def main_script():
+    user_id = input("Masukkan user ID: ")  # Meminta user_id dari pengguna
+    await main('proxy_3.txt', user_id)
+
+if __name__ == "__main__":
+    asyncio.run(main_script())
