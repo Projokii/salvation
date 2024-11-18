cf1eac798f1efe51da0dfd3c7a5ade0f27716986
Projokii <141165642+Projokii@users.noreply.github.com>
2024-11-18 21:14:23 +0700

    Update 4.py

--- 4.py
+++ 4.py
@@ -2,8 +2,14 @@ import asyncio
 from proxy_handler import main
 
 async def main_script():
-    user_id = input("Masukkan user ID: ")  # Meminta user_id dari pengguna
-    await main('proxy_4.txt', user_id)
+    try:
+        with open('userid.txt', 'r') as f:
+            user_id = f.read().strip()  # Membaca user_id dari file
+    except FileNotFoundError:
+        print("File 'userid.txt' tidak ditemukan.")
+        return
+
+    await main('proxy_4.txt', user_id)  # Menggunakan proxy_4.txt untuk skrip 4.py
 
 if __name__ == "__main__":
     asyncio.run(main_script())
